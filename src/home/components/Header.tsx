2b1bdb370ce6fa34005258c34b5e3cb41d7d4d04
Julien <julien.izzillo@gmail.com>
2023-01-30 20:12:26 +0100

    Adding centered header text with style. The text can be modified with a prop.

--- src/home/components/Header.tsx
+++ src/home/components/Header.tsx
@@ -3,9 +3,14 @@ import React from 'react';
 
 const HEADER_HEIGHT = 60;
 const HEADER_BOTTOM_MARGIN = 20;
+const HEADER_FONT_SIZE = 30;
 
-export const Header = () => {
-  return <View style={styles.container}></View>;
+export const Header = (headerText: string) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>{headerText}</Text>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
@@ -13,5 +18,11 @@ const styles = StyleSheet.create({
     width: '100%',
     height: HEADER_HEIGHT,
     marginBottom: HEADER_BOTTOM_MARGIN,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: HEADER_FONT_SIZE,
+    fontWeight: 'bold',
   },
 });
