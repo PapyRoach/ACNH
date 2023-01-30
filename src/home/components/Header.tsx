9bcc9020532431bef82b98625d4e2ca042db7c43
Julien <julien.izzillo@gmail.com>
2023-01-30 20:04:34 +0100

    Setting dimensions of Header wrapping view

--- src/home/components/Header.tsx
+++ src/home/components/Header.tsx
@@ -1,8 +1,17 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 
+const HEADER_HEIGHT = 60;
+const HEADER_BOTTOM_MARGIN = 20;
+
 export const Header = () => {
-  return <View></View>;
+  return <View style={styles.container}></View>;
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    height: HEADER_HEIGHT,
+    marginBottom: HEADER_BOTTOM_MARGIN,
+  },
+});
