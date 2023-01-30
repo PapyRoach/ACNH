395c1780f921b53d06403dfa786a784b76b6c781
Julien <julien.izzillo@gmail.com>
2023-01-30 19:22:20 +0100

    Changing App function to return HomeScreen comonent instead of empty view

--- App.tsx
+++ App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import {View} from 'react-native';
+import {HomeScreen} from './src/home/HomeScreen';
 
 const App = () => {
-  return <View></View>;
+  return <HomeScreen />;
 };
 
 export default App;
