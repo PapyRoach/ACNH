51d2462b0437e675778daf78a095d2a1eb33c7c7
Julien <julien.izzillo@gmail.com>
2023-01-31 13:57:02 +0100

    Showing button on home screen

--- src/home/HomeScreen.tsx
+++ src/home/HomeScreen.tsx
@@ -1,6 +1,7 @@
 import {View} from 'react-native';
 import React from 'react';
 import {Header} from './components/Header';
+import {HomeButton} from './components/HomeButton';
 
 export const HomeScreen = () => {
   return (
@@ -9,6 +10,12 @@ export const HomeScreen = () => {
         headerImagePath={require('./components/img/headerBackground.jpg')}
         headerText="Companion"
       />
+      <HomeButton
+        label="Tracker"
+        clickHandler={() => {
+          console.log('Appui !');
+        }}
+        imgPath={require('./src/home/components/img/tracker.png')}></HomeButton>
     </View>
   );
 };
