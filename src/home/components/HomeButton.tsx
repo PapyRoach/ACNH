138a6bc4d81e10dac403f0b5a85760e4abffc380
Julien <julien.izzillo@gmail.com>
2023-01-31 12:02:45 +0100

    Adding style to the view crapping button elements (setting height, width and alignement)

--- src/home/components/HomeButton.tsx
+++ src/home/components/HomeButton.tsx
@@ -1,8 +1,17 @@
 import {StyleSheet, View} from 'react-native';
 import React from 'react';
 
+const BUTTON_TOTAL_HEIGHT = 160;
+const BUTTON_TOTAL_WIDTH = 120;
+
 export const HomeButton = () => {
-  return <View></View>;
+  return <View style={styles.container}></View>;
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    height: BUTTON_TOTAL_HEIGHT,
+    width: BUTTON_TOTAL_WIDTH,
+  },
+});
