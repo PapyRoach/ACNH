e39c1d852bf83e0a329522777995e868a5f8d1cc
Julien <julien.izzillo@gmail.com>
2023-01-31 12:11:26 +0100

    Adding style to TouchableOpacity

--- src/home/components/HomeButton.tsx
+++ src/home/components/HomeButton.tsx
@@ -3,10 +3,11 @@ import React from 'react';
 
 const BUTTON_TOTAL_HEIGHT = 160;
 const BUTTON_TOTAL_WIDTH = 120;
+const BUTTON_ICON_HEIGHT_AND_WIDTH = 100;
 
 export const HomeButton = () => {
   <View style={styles.container}>
-    <TouchableOpacity></TouchableOpacity>
+    <TouchableOpacity style={styles.touchable}></TouchableOpacity>
   </View>;
 };
 
@@ -15,5 +16,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     height: BUTTON_TOTAL_HEIGHT,
     width: BUTTON_TOTAL_WIDTH,
+  },
+  touchable: {
+    height: BUTTON_ICON_HEIGHT_AND_WIDTH,
+    width: BUTTON_ICON_HEIGHT_AND_WIDTH,
   },
 });
