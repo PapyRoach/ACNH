fb9eb2f44be8127d0bfe3ddcea8e2ff97c6d7aba
Julien <julien.izzillo@gmail.com>
2023-02-09 15:35:19 +0100

    Adding buttons data set for flatlist use. The data set specifies labels and icons

---
+++ src/home/components/HomeButtonsData.tsx
@@ -0,0 +1,18 @@
+export const HOME_SCREEN_BUTTONS_DATASET = [
+  {
+    label: 'Encyclopédie',
+    imgPath: require('./img/encyclo_betes.png'),
+  },
+  {
+    label: 'Tracker',
+    imgPath: require('./img/tracker.png'),
+  },
+  {
+    label: 'Fossiles',
+    imgPath: require('./img/encyclo_fossiles.png'),
+  },
+  {
+    label: 'Mon Village',
+    imgPath: require('./img/my_village.png'),
+  },
+];
