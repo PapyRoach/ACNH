a467e79dfc2e5dc72bd23d2608b073d15bc586e5
Julien <julien.izzillo@gmail.com>
2023-02-09 15:43:42 +0100

    Adding FlatList that generates buttons on HomeScreen according to buttons data

--- src/home/HomeScreen.tsx
+++ src/home/HomeScreen.tsx
@@ -1,8 +1,10 @@
-import {View} from 'react-native';
+import {View, FlatList} from 'react-native';
 import React from 'react';
 import {Header} from './components/Header';
 import {HomeButton} from './components/HomeButton';
 import {HOME_SCREEN_BUTTONS_DATASET} from './components/HomeButtonsData';
+
+export const NUMBER_OF_COLUMNS = 3;
 
 export const HomeScreen = () => {
   return (
@@ -11,6 +13,19 @@ export const HomeScreen = () => {
         headerImagePath={require('./components/img/headerBackground.jpg')}
         headerText="Companion"
       />
+      <View style={styles.listWrapper}>
+        <FlatList
+          horizontal={false}
+          numColumns={NUMBER_OF_COLUMNS}
+          data={HOME_SCREEN_BUTTONS_DATASET}
+          renderItem={button => (
+            <HomeButton
+              label={button.item.label}
+              imgPath={button.item.imgPath}
+            />
+          )}
+        />
+      </View>
     </View>
   );
 };
