ac76495d3f8a41ed6cbc8e492f7cf444202d5708
Julien <julien.izzillo@gmail.com>
2023-02-14 20:55:18 +0100

    Adding Encyclopedie to the navigation stack

--- src/navigation/Navigator.tsx
+++ src/navigation/Navigator.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {HomeScreen} from '../pages/home/HomeScreen';
+import {Encyclopedie} from '../pages/encyclopedie/Encyclopedie';
 
 const Stack = createNativeStackNavigator();
 
@@ -13,6 +14,7 @@ export const Navigator = () => {
           headerShown: false,
         }}>
         <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Encyclopédie" component={Encyclopedie} />
       </Stack.Navigator>
     </NavigationContainer>
   );
