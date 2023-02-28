c78343d92409b228d118d485b57ddc928d4017c2
Julien <julien.izzillo@gmail.com>
2023-02-28 22:54:07 +0100

    Changing from navigation prop to useNavigation hook and adding appropriate typing

--- src/navigation/Types.tsx
+++ src/navigation/Types.tsx
@@ -1,4 +1,8 @@
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
+
 export type RootStackParamList = {
   Home: undefined;
   Encyclopedie: undefined;
 };
+
+export type NavigationProp = NativeStackNavigationProp<RootStackParamList>;
