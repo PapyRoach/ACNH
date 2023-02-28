43ef7406f9e3db16614457534db1075c29f8ac43
Julien <julien.izzillo@gmail.com>
2023-02-28 22:54:07 +0100

    Changing imgPath to imagePath in prop naming for HomeButton

--- src/pages/home/components/HomeButtonsData.tsx
+++ src/pages/home/components/HomeButtonsData.tsx
@@ -3,22 +3,22 @@ import {ButtonData} from './HomeButton';
 export const HOME_SCREEN_BUTTONS_DATASET: ButtonData[] = [
   {
     label: 'Encyclopédie',
-    imgPath: require('../img/encyclo_betes.png'),
+    imagePath: require('../img/encyclo_betes.png'),
     navigationTarget: 'Encyclopedie',
   },
   {
     label: 'Tracker',
-    imgPath: require('../img/tracker.png'),
+    imagePath: require('../img/tracker.png'),
     navigationTarget: 'Tracker',
   },
   {
     label: 'Fossiles',
-    imgPath: require('../img/encyclo_fossiles.png'),
+    imagePath: require('../img/encyclo_fossiles.png'),
     navigationTarget: 'Fossiles',
   },
   {
     label: 'Mon Village',
-    imgPath: require('../img/my_village.png'),
+    imagePath: require('../img/my_village.png'),
     navigationTarget: 'MonVillage',
   },
 ];
