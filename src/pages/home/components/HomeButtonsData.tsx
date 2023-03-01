294a7218010e58d7dad958493ead8e1c3c71ca42
Julien <julien.izzillo@gmail.com>
2023-03-01 17:56:45 +0100

    Deleting dead code

--- src/pages/home/components/HomeButtonsData.tsx
+++ src/pages/home/components/HomeButtonsData.tsx
@@ -6,19 +6,4 @@ export const HOME_SCREEN_BUTTONS_DATASET: ButtonData[] = [
     imagePath: require('../img/encyclo_betes.png'),
     navigationTarget: 'Encyclopedie',
   },
-  {
-    label: 'Tracker',
-    imagePath: require('../img/tracker.png'),
-    navigationTarget: 'Tracker',
-  },
-  {
-    label: 'Fossiles',
-    imagePath: require('../img/encyclo_fossiles.png'),
-    navigationTarget: 'Fossiles',
-  },
-  {
-    label: 'Mon Village',
-    imagePath: require('../img/my_village.png'),
-    navigationTarget: 'MonVillage',
-  },
 ];
