4b06c47e132088235cf8e10f484b08ec1fb56be9
Julien <julien.izzillo@gmail.com>
2023-03-07 22:41:39 +0100

    Setting color for typography components

--- src/theme/typography/Variants.tsx
+++ src/theme/typography/Variants.tsx
@@ -4,12 +4,15 @@ export const typos: Record<TypographyType, any> = {
   header: {
     fontSize: 30,
     fontWeight: 'bold',
+    color: 'black',
   },
   iconLabel: {
     textAlign: 'center',
     fontSize: 14,
+    color: 'black',
   },
   'simple-text': {
     fontSize: 14,
+    color: 'black',
   },
 };
